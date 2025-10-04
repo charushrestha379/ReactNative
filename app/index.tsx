5220204ca5646a7c1b7e6aa13f322432ae92e5f8
charushrestha379 <charushrestha776@gmail.com>
2025-10-04 22:37:39 +0545

    react native fundamentals

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,19 @@
-import React from "react";
-import { View, Text, Image, ScrollView, TextInput } from "react-native";
+import React from 'react';
+import {View, Text, ScrollView, TextInput, Button} from 'react-native';
 
-const App = () => {
-  return (
-    <ScrollView className="flex-1 p-4">
-      <Text className="text-lg font-bold">Some text</Text>
-
-      <View className="mt-4">
-        <Text className="text-base">Some more text</Text>
-        <Image
-          source={{
-            uri: "https://reactnative.dev/docs/assets/p_cat2.png",
-          }}
-          className="w-52 h-52 rounded-xl"
-        />
+const app = () => {
+  return(
+    <ScrollView>
+    <View>
+      <Text>Hello World</Text>
+      <TextInput placeholder= "Type here"/>
+      <View  style={{margin: 20}}>
+      <Button title="press me" onPress ={() => {}}/>
       </View>
-
-      <TextInput
-        className="h-10 border border-gray-400 rounded-md mt-4 px-2"
-        defaultValue="You can type in me"
-      />
+    </View>
     </ScrollView>
-  );
+  
+);
 };
 
-export default App;
+export default app;
